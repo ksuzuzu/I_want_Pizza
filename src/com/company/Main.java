0ecf9df25075eca807c5366cc7cda6e8a9b0293a
asus <ksucheersport@yandex.ru>
2021-03-29 07:24:44 +0300

    1.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        String yes = "да";
+        String not = "нет";
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Хотите пиццу? Введите да или нет:");
+            String text = scanner.nextLine();
+            String t = text.toLowerCase();
+
+
+            if (t.equals(yes)) {
+                System.out.println("Готовлю пиццу...");
+                Thread.sleep(5000);
+                System.out.println("Пицца приготовлена, кушать подано");
+                break;
+
+            }
+            if (t.equals(not)) {
+                System.out.println("До свидания");
+                System.out.println("))))");
+                break;
+            } else System.out.println("Некорректный ввод");
+
+
+        }
+
+    }
+    }
+
+
+
+
